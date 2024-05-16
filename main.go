a44f3a9b60a647b755541825e01cb6120d8c13d5
kota-es <info@k-3110.com>
2024-05-16 23:12:52 +0900

    feat: create handlers

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"myapi/handlers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/hello", handlers.HandleHanlder).Methods(http.MethodGet)
+    r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+    r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+    r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+    r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+    r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
+    log.Println("server start at port 8080")
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
