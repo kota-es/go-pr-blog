8ef67d03d6840291a683b3e65fc51af2178ac973
kota-es <info@k-3110.com>
2024-05-21 20:20:22 +0900

    feat: create models

---
+++ models/data.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"time"
+)
+
+var (
+	Comment1 = Comment{
+		CommentID: 1,
+		AritcleID: 1,
+		Message:   "test comment1",
+		CreateAt:  time.Now(),
+	}
+
+	Comment2 = Comment{
+		CommentID: 2,
+		AritcleID: 1,
+		Message:   "test comment2",
+		CreateAt:  time.Now(),
+	}
+)
+
+var (
+	Article1 = Article{
+		ID:          1,
+		Title:       "first article",
+		Contents:    "This is a first article",
+		UserName:    "saki",
+		NiceNum:     1,
+		CommentList: []Comment{Comment1, Comment2},
+		CreatedAt:   time.Now(),
+	}
+
+	Article2 = Article{
+		ID:        2,
+		Title:     "second article",
+		Contents:  "This is a second article",
+		UserName:  "saki",
+		NiceNum:   2,
+		CreatedAt: time.Now(),
+	}
+)
