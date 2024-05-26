a1b427b5630907bdacad8d76f0530c44e4b81733
kota-es <info@k-3110.com>
2024-05-26 20:17:41 +0900

    refar: db connection di

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
-	"myapi/handlers"
+	"myapi/controllers"
+	"myapi/services"
 	"net/http"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
@@ -17,14 +22,28 @@ func init() {
 }
 
 func main() {
+	dbUser := os.Getenv("USERNAME")
+	dbPass := os.Getenv("USERPASS")
+	dbDatabse := os.Getenv("DATABASE")
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1)/%s?parseTime=true", dbUser, dbPass, dbDatabse)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("fail to connect db")
+		return
+	}
+
+	ser := services.NewMyAppService(db)
+	con := controllers.NewMyAppController(ser)
+
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HandleHanlder).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/hello", con.HelloHanlder).Methods(http.MethodGet)
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
