18949558fa77a8fa1cae28b840efe43b4c1b967b
kota-es <info@k-3110.com>
2024-06-02 15:12:58 +0900

    feat: use context for trace log

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -1,10 +1,12 @@
 package middlewares
 
 import (
+	"context"
 	"log"
 	"net/http"
 )
 
+type traceIDKey struct{}
 type resLoggingWriter struct {
 	http.ResponseWriter
 	code int
@@ -21,13 +23,31 @@ func (rsw *resLoggingWriter) WriteHeader(code int) {
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		traceID := newTraceID()
 		// リクエスト情報をロギング
-		log.Println(req.RequestURI, req.Method)
+		log.Printf("[%d]%s %s\n", traceID, req.Method, req.URL.Path)
 
+		ctx := SetTraceID(req.Context(), traceID)
+		SetTraceID(ctx, traceID)
+		req = req.WithContext(ctx)
 		rlw := NewResLoggingWriter(w)
 
 		next.ServeHTTP(rlw, req)
 
-		log.Println("res: ", rlw.code)
+		log.Printf("[%d]res: %d\n", traceID, rlw.code)
 	})
+}
+
+func SetTraceID(ctx context.Context, traceID int) context.Context {
+	return context.WithValue(ctx, traceIDKey{}, traceID)
+}
+
+func GetTraceID(ctx context.Context) int {
+	id := ctx.Value(traceIDKey{})
+
+	if idInt, ok := id.(int); ok {
+		return idInt
+	}
+
+	return 0
 }
